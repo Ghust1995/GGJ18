a61536dbc3569f4a6a72f7aed6137253ce15056c
MAD4R4 <wagner_madara@hotmail.com>
2018-01-28 00:30:36 -0200

    adicionado mini AI

--- Assets/NPCMovement.cs
+++ Assets/NPCMovement.cs
@@ -8,12 +8,14 @@ public class NPCMovement : MonoBehaviour
   GameObject Player;
   Rigidbody2D rb;
   public Vector2 Destination;
+    private Transform Target;
   private Mood currentMood;
   LayerMask mask = 0;
 
-  public bool Angry = false;
-  private bool isFinding = false;
-  private MoodBehaviour[] col;
+   // bool tempDestiny = false;
+    public bool Angry = false;
+  private bool HaveTarget = false;
+  private Collider [] col;
   public Vector2 VelocityNPC = new Vector2(1, 0.5f);
   private Vector2 roamingArea;
 
@@ -21,7 +23,7 @@ public class NPCMovement : MonoBehaviour
   // Use this for initialization
   void Start()
   {
-    rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     Destination = Random.insideUnitCircle * 5 + new Vector2(transform.position.x, transform.position.y);
     Player = GameObject.FindGameObjectWithTag("Player");
     roamingArea = FindObjectOfType<WorldData>().RoamingArea;
@@ -31,25 +33,44 @@ public class NPCMovement : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    currentMood = GetComponent<MoodBehaviour>().currentMood;
+        currentMood = GetComponent<MoodBehaviour>().currentMood;
 
-    var direction = -((Vector2)transform.position - Destination).normalized;
-    transform.position += (Vector3) (Vector2.Scale(direction, VelocityNPC) * Time.deltaTime);
+        var direction = -((Vector2)transform.position - Destination).normalized;
+        transform.position += (Vector3) (Vector2.Scale(direction, VelocityNPC) * Time.deltaTime);
 
-    if (Vector2.Distance(Destination, transform.position) < 0.1) // && currentMood == Mood.Neutral)
-    {
-      SetNextDestination();
-    }
+        if (  currentMood == Mood.Neutral && Vector2.Distance(Destination, transform.position) < 0.1)
+        {
+          SetNextDestination();
+        }
+
+
+        if (currentMood == Mood.Angry && HaveTarget == false)
+            {
+                SearchTarget();
+            }
+        else if (currentMood == Mood.Angry && HaveTarget == true)
+            {
+                AttackTarget();
+            }
+
+       
+
+
   }
 
   void OnDrawGizmos() {
       Gizmos.color = Color.white;
       Gizmos.DrawLine(transform.position, Destination);
-  }
+       
+    }
 
   void SetNextDestination()
   {
-    Destination = RandomPointInSquare(Vector2.zero, roamingArea);
+            
+        
+            
+            Destination = RandomPointInSquare(Vector2.zero, roamingArea);
+        
   }
 
   Vector2 RandomPointInSquare(Vector2 center, Vector2 size){
@@ -58,46 +79,112 @@ public class NPCMovement : MonoBehaviour
                 (Random.value - 0.5f) * size.y);
   }
 
-  
 
-  private IEnumerator Find()
+
+  private void SearchTarget()
   {
-    isFinding = true;
-    yield return new WaitForSeconds(1);
-    AngerTarget();
-    Debug.Log("Quantas vezes ");
-    isFinding = false;
+        Collider2D [] col = Physics2D.OverlapCircleAll(transform.position, 3 );
+
+            for (int i =0; i<col.Length ; i++)
+            {
+
+
+                if (col[i].gameObject == gameObject)
+                {
+                        continue;
+                }
+
+                if (col[i].tag == "Player"  && col[i].GetComponent<IsTarget>().isTarget == false)
+                {
+                        col[i].GetComponent<IsTarget>().isTarget = true;
+                        Destination = col[i].transform.position;
+                        Target = col[i].transform;
+                        HaveTarget = true;
+                        break;
+                }
+
+                if (col[i].tag == "NPC" && col[i].GetComponent<MoodBehaviour>().currentMood == Mood.Neutral
+                    && col[i].GetComponent<IsTarget>().isTarget == false)
+                    {
+                        col[i].GetComponent<IsTarget>().isTarget = true;
+                        Destination = col[i].transform.position;
+                        Target = col[i].transform;
+                        HaveTarget = true;
+                        break;
+
+                    }
+
+                if (col[i].tag == "NPC" && col[i].GetComponent<MoodBehaviour>().currentMood == Mood.Neutral
+                && col[i].GetComponent<IsTarget>().isTarget == true)
+                {
+                    continue;
+
+                }
+
+                if (i == col.Length - 1)
+                {
+                    if (Vector2.Distance(transform.position, Destination) < 2)
+                    {
+                        SetNextDestination();
+                    }
+                    break;
+                }
+
+                if (col[i].tag == "NPC" && col[i].GetComponent<MoodBehaviour>().currentMood == Mood.Angry)
+                        {
+                            continue;
+                        }
+
+                
+            }
+        
+        
+
+            
+        
+       
   }
 
-  void AngerTarget()
-  {
-    if (Vector2.Distance(Player.transform.position, transform.position) < 3)
+    private void AttackTarget ()
     {
-      Destination = Player.transform.position;
-    }
-    else
-    {
-      Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, 2, mask.value);
-
-      if (col.Length != 1)
-      {
-        for (int i = 0; i < col.Length; i++)
+        if (Target != null)
         {
-          if (col[i].gameObject == gameObject)
-          {
-            continue;
-          }
 
-          if (col[i].tag == "NPC" && col[i].GetComponent<MoodBehaviour>().currentMood == Mood.Neutral)
-          {
-            Destination = col[i].transform.position;
-            break;
-          }
+            if (Target.tag == "NPC")
+            {
+                if (Target.GetComponent<MoodBehaviour>().currentMood != Mood.Neutral)
+                {
+                    HaveTarget = false;
+                    Target = null;
+                }
+                else
+                {
+                    Destination = Target.transform.position;
+                }
+            }
+
+            else if (Target.tag == "Player")
+            {
+                if (Vector2.Distance(transform.position, Target.position) < 4)
+                {
+                    Destination = Target.position;
+                }
+                else
+                {
+                    HaveTarget = false;
+                    Target = null;
+                }
+            }
 
         }
-      }
+        
+       
+
     }
-  }
+
+
+
+
 }
 
 
